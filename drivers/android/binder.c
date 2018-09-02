ff152d6f395b728b3df62104cbd2c779a6c2e9e9
marc0601 <marckhalil06@gmail.com>
2018-09-02 17:20:11 +0200

    Revert "patch based on https://patches.linaro.org/patch/25604/"
    This reverts commit c0df839d0edb6a80ab3308b1d3f79adc80b72518.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3452,14 +3452,10 @@ static int binder_thread_write(struct binder_proc *proc,
 			struct binder_node *node;
 			bool free_node;
 
-			if (copy_from_user(&node_ptr,
-					   (binder_uintptr_t __user *)ptr,
-					   sizeof(binder_uintptr_t)))
+			if (get_user(node_ptr, (binder_uintptr_t __user *)ptr))
 				return -EFAULT;
 			ptr += sizeof(binder_uintptr_t);
-			if (copy_from_user(&cookie,
-					   (binder_uintptr_t __user *)ptr,
-					   sizeof(binder_uintptr_t)))
+			if (get_user(cookie, (binder_uintptr_t __user *)ptr))
 				return -EFAULT;
 			ptr += sizeof(binder_uintptr_t);
 			node = binder_get_node(proc, node_ptr);
@@ -3528,9 +3524,7 @@ static int binder_thread_write(struct binder_proc *proc,
 			binder_uintptr_t data_ptr;
 			struct binder_buffer *buffer;
 
-			if (copy_from_user(&data_ptr,
-					   (binder_uintptr_t __user *)ptr,
-					   sizeof(binder_uintptr_t)))
+			if (get_user(data_ptr, (binder_uintptr_t __user *)ptr))
 				return -EFAULT;
 			ptr += sizeof(binder_uintptr_t);
 
@@ -3652,9 +3646,7 @@ static int binder_thread_write(struct binder_proc *proc,
 			if (get_user(target, (uint32_t __user *)ptr))
 				return -EFAULT;
 			ptr += sizeof(uint32_t);
-			if (copy_from_user(&cookie,
-					   (binder_uintptr_t __user *)ptr,
-					   sizeof(binder_uintptr_t)))
+			if (get_user(cookie, (binder_uintptr_t __user *)ptr))
 				return -EFAULT;
 			ptr += sizeof(binder_uintptr_t);
 			if (cmd == BC_REQUEST_DEATH_NOTIFICATION) {
@@ -3774,9 +3766,7 @@ static int binder_thread_write(struct binder_proc *proc,
 			binder_uintptr_t cookie;
 			struct binder_ref_death *death = NULL;
 
-			if (copy_from_user(&cookie,
-					   (binder_uintptr_t __user *)ptr,
-					   sizeof(binder_uintptr_t)))
+			if (get_user(cookie, (binder_uintptr_t __user *)ptr))
 				return -EFAULT;
 
 			ptr += sizeof(cookie);
